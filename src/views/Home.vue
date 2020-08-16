5184f57a6f8b11142b32952480fdfffd626c56df
Eduardo Sanchez <esanchezh@unsa.edu.pe>
2020-08-16 03:43:23 -0500

    links menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h1 class="text-center">Bienvenido Usuario</h1>
-    <router-link class="hover:underline text-green-600" :to="{ name: 'roles' }"
-      >Roles</router-link
-    >
+    <ul>
+      <li>
+        <router-link class="hover:underline text-green-600" :to="{ name: 'roles' }">Roles</router-link>
+      </li>
+      <li>
+        <router-link class="hover:underline text-green-600" :to="{ name: 'pabellon' }">Pabellon</router-link>
+      </li>
+      <li>
+        <router-link class="hover:underline text-green-600" :to="{ name: 'aulas' }">Aulas</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
